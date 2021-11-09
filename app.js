34a0cac6a184d7f4d3ea950c66cfcb4773ecda0c
wilmergarcia7 <wilmergarciap1@gmail.com>
2021-11-08 19:47:53 -0600

    dependencias agregadas

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
+//app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'whatever', resave: true, saveUninitialized: true }));
 
